88d8ce7dc2dca09ee459568f2b256f672c8ce4c2
Jing Wang <99jingw@gmail.com>
2020-05-02 12:26:40 -0400

    Reformat code using black

--- shellquery.py
+++ shellquery.py
@@ -8,7 +8,7 @@ import sqlite3
 import sys
 import tempfile
 
-__version__ = '0.1.4'
+__version__ = "0.1.4"
 
 from sqlite3 import Cursor
 from sqlite3 import Connection
@@ -26,19 +26,19 @@ _logger = logging.getLogger(__name__)
 
 EXAMPLES = [
     (
-        'Selecting columns',
+        "Selecting columns",
         r"""echo -e '1 2 3\n4 5 6' | {name} 'SELECT c3, c1 FROM "-"'""",
-        '3\t1\n6\t4\n'
+        "3\t1\n6\t4\n",
     ),
     (
-        'Syntax shortcut (SELECT and FROM optional)',
+        "Syntax shortcut (SELECT and FROM optional)",
         r"echo -e '1 2 3\n4 5 6' | {name} c3,c1",
-        '3\t1\n6\t4\n'
+        "3\t1\n6\t4\n",
     ),
     (
-        'Joining files with stdin (suppose web.log has userid,path and users has userid,name)',
+        "Joining files with stdin (suppose web.log has userid,path and users has userid,name)",
         """cat web.log | {name} 'SELECT "-".c2, users.c2 FROM "-" JOIN users ON "-".c1 = users.c1'""",  # noqa
-        "/some/path\talice\n/foo/bar\tbob\n/blah/blah\tbob\n"
+        "/some/path\talice\n/foo/bar\tbob\n/blah/blah\tbob\n",
     ),
 ]
 
@@ -46,12 +46,12 @@ EXAMPLES = [
 def format_examples() -> str:
     lines = []
     for name, cmd, result in EXAMPLES:
-        lines.append('  ' + name + ':')
-        lines.append('    $ ' + cmd.format(name=os.path.basename(sys.argv[0])))
+        lines.append("  " + name + ":")
+        lines.append("    $ " + cmd.format(name=os.path.basename(sys.argv[0])))
         for line in result.splitlines():
-            lines.append('    ' + line)
-        lines.append('')
-    return '\n'.join(lines)
+            lines.append("    " + line)
+        lines.append("")
+    return "\n".join(lines)
 
 
 HELP_TEXT = """\
@@ -59,7 +59,9 @@ ShellQuery {version}: Command line SQL on plain text files and standard input.
 
 Example usage:
 {examples}
-""".format(version=__version__, examples=format_examples())
+""".format(
+    version=__version__, examples=format_examples()
+)
 
 QUERY_HELP = """\
 A SQL query to run. \
@@ -81,43 +83,67 @@ LOAD_ROWS_MAX_BUFFER = 1000
 def main() -> None:
     logging.basicConfig()
     parser = argparse.ArgumentParser(
-        description=HELP_TEXT,
-        formatter_class=argparse.RawDescriptionHelpFormatter
+        description=HELP_TEXT, formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    parser.add_argument('query', help=QUERY_HELP)
-    parser.add_argument('-d', '--delimiter', default=r'\s+',
-                        help="A regular expression for splitting lines into columns. "
-                             "Defaults to whitespace.")
-    parser.add_argument('-F', '--fixed-string', action='store_true', default=False,
-                        help="Interpret delimiter as a fixed string instead of a regex")
-    parser.add_argument('-c', '--max-columns', type=int, default=100,
-                        help="Maximum number of columns to store. "
-                             "If splitting by the delimiter would produce too many columns, the "
-                             "final column will be the entire remainder of the string. "
-                             "Passing \"1\" lets you query on whole lines instead of columns. "
-                             "Note that SQLite bombs out after 999 columns.")
-    parser.add_argument('--output-delimiter', default='\t',
-                        help="String to use to separate columns. "
-                             "Defaults to tab.")
-    parser.add_argument('-H', '--output-header', action='store_true', default=False,
-                        help="Include a header row in the output")
+    parser.add_argument("query", help=QUERY_HELP)
+    parser.add_argument(
+        "-d",
+        "--delimiter",
+        default=r"\s+",
+        help="A regular expression for splitting lines into columns. Defaults to whitespace.",
+    )
+    parser.add_argument(
+        "-F",
+        "--fixed-string",
+        action="store_true",
+        default=False,
+        help="Interpret delimiter as a fixed string instead of a regex",
+    )
+    parser.add_argument(
+        "-c",
+        "--max-columns",
+        type=int,
+        default=100,
+        help=(
+            "Maximum number of columns to store. "
+            "If splitting by the delimiter would produce too many columns, "
+            "the final column will be the entire remainder of the string. "
+            'Passing "1" lets you query on whole lines instead of columns. '
+            "Note that SQLite bombs out after 999 columns."
+        ),
+    )
+    parser.add_argument(
+        "--output-delimiter",
+        default="\t",
+        help="String to use to separate columns. Defaults to tab.",
+    )
+    parser.add_argument(
+        "-H",
+        "--output-header",
+        action="store_true",
+        default=False,
+        help="Include a header row in the output",
+    )
     args = parser.parse_args()
 
-    results = execute_query(args.query, args.delimiter, args.max_columns, args.fixed_string)
+    results = execute_query(
+        args.query, args.delimiter, args.max_columns, args.fixed_string
+    )
     print_output(results, args.output_delimiter, args.output_header)
 
 
-def load_file(connection: Connection,
-              table_name: str,
-              delimiter: str,
-              max_columns: int,
-              fixed_string: bool,
-              ) -> None:
+def load_file(
+    connection: Connection,
+    table_name: str,
+    delimiter: str,
+    max_columns: int,
+    fixed_string: bool,
+) -> None:
     def load(file: IO[str]) -> None:
         rows = read_columns(file, delimiter, max_columns, fixed_string)
         load_rows(connection, table_name, rows)
 
-    if table_name == '-':
+    if table_name == "-":
         load(sys.stdin)
     else:
         with open(table_name) as f:
@@ -129,7 +155,7 @@ def re_split(regex: Pattern[str], string: str, maxsplit: int) -> List[str]:
 
     https://docs.python.org/3/library/re.html#re.split
     """
-    if regex.match(''):
+    if regex.match(""):
         raise ValueError("Delimiter matching empty string not supported")
     assert maxsplit > 0
     parts = []
@@ -142,19 +168,18 @@ def re_split(regex: Pattern[str], string: str, maxsplit: int) -> List[str]:
         if len(parts) >= maxsplit:
             break
     assert cur <= len(string)
-    parts.append(string[cur:])  # can be empty if string is empty or there's a match at the end
+    # can be empty if string is empty or there's a match at the end
+    parts.append(string[cur:])
     return parts
 
 
-def read_columns(file: Iterable[str],
-                 delimiter: str,
-                 max_columns: int,
-                 fixed: bool,
-                 ) -> Iterator[List[str]]:
+def read_columns(
+    file: Iterable[str], delimiter: str, max_columns: int, fixed: bool,
+) -> Iterator[List[str]]:
     """Yield the rows/columns in the given file as a list of lists"""
     col_regex = re.compile(re.escape(delimiter) if fixed else delimiter)
     for line in file:
-        if line.endswith('\n'):
+        if line.endswith("\n"):
             line = line[:-1]
         if line:
             if max_columns > 1:
@@ -165,20 +190,24 @@ def read_columns(file: Iterable[str],
             yield []
 
 
-def load_rows(connection: Connection, table: str, data: Iterable[Sequence[object]]) -> None:
+def load_rows(
+    connection: Connection, table: str, data: Iterable[Sequence[object]]
+) -> None:
     """Create `table` from the given iterable of rows `data`"""
     cur_width = 1
-    col_fmt = 'c{} TEXT'
-    create_table_stmt = 'CREATE TABLE {} ({})'.format(
-        quote_identifier(table), col_fmt.format(1))
+    col_fmt = "c{} TEXT"
+    create_table_stmt = "CREATE TABLE {} ({})".format(
+        quote_identifier(table), col_fmt.format(1)
+    )
     connection.execute(create_table_stmt)
 
     current_rows: List[List[object]] = []
 
     def flush() -> None:
-        placeholders = ','.join('?' * cur_width)
-        insert_query = 'INSERT INTO {} VALUES ({})'.format(
-            quote_identifier(table), placeholders)
+        placeholders = ",".join("?" * cur_width)
+        insert_query = "INSERT INTO {} VALUES ({})".format(
+            quote_identifier(table), placeholders
+        )
         connection.executemany(insert_query, current_rows)
         del current_rows[:]
 
@@ -189,8 +218,9 @@ def load_rows(connection: Connection, table: str, data: Iterable[Sequence[object
                 flush()
             # sqlite alter table takes constant time, regardless of data already in the table
             # https://www.sqlite.org/lang_altertable.html
-            alter_table_statement = 'ALTER TABLE {} ADD COLUMN {}'.format(
-                quote_identifier(table), col_fmt.format(cur_width + 1))
+            alter_table_statement = "ALTER TABLE {} ADD COLUMN {}".format(
+                quote_identifier(table), col_fmt.format(cur_width + 1)
+            )
             connection.execute(alter_table_statement)
             cur_width += 1
 
@@ -204,44 +234,46 @@ def load_rows(connection: Connection, table: str, data: Iterable[Sequence[object
 def add_from_clause(query: str, table: str) -> str:
     """If the query doesn't have a FROM clause, add it using the given table."""
     # Note: doesn't work when query has FROM, GROUP BY, or ORDER BY as a non-keyword
-    if re.search(r'\bFROM\b', query, re.I):
+    if re.search(r"\bFROM\b", query, re.I):
         # already has a from clause
         return query
     else:
-        clause = 'FROM {} '.format(quote_identifier(table))
+        clause = "FROM {} ".format(quote_identifier(table))
         # These are in order of how they should appear in a proper SQL statement
-        key_words = [r'WHERE', r'GROUP\s+BY', r'ORDER\s+BY', 'LIMIT']
+        key_words = [r"WHERE", r"GROUP\s+BY", r"ORDER\s+BY", "LIMIT"]
         for word in key_words:
-            match = re.search(r'\b' + word + r'\b', query, re.I)
+            match = re.search(r"\b" + word + r"\b", query, re.I)
             if match:
                 # Insert FROM clause before the group/order clause
                 start = match.start()
                 return query[:start] + clause + query[start:]
         # didn't find GROUP BY or ORDER BY clause, so append to end
-        return query + ' ' + clause
+        return query + " " + clause
 
 
 def add_select(query: str) -> str:
     """If the query doesn't start with SELECT or WITH, add it."""
     # Note: doesn't work if there are comments in the beginning of the query
-    if re.match(r'\s*(SELECT|WITH)\b', query, re.I):
+    if re.match(r"\s*(SELECT|WITH)\b", query, re.I):
         return query
     else:
-        return 'SELECT ' + query
+        return "SELECT " + query
 
 
 def quote_identifier(name: str) -> str:
     if '"' not in name:
         return '"' + name + '"'
-    if '`' not in name:
-        return '`' + name + '`'
-    if ']' not in name:
-        return '[' + name + ']'
+    if "`" not in name:
+        return "`" + name + "`"
+    if "]" not in name:
+        return "[" + name + "]"
     raise ValueError("Unsupported identifier: {}".format(name))
 
 
-def execute_query(query: str, delimiter: str, max_columns: int, fixed_string: bool) -> Cursor:
-    processed_query = add_from_clause(add_select(query), '-')
+def execute_query(
+    query: str, delimiter: str, max_columns: int, fixed_string: bool
+) -> Cursor:
+    processed_query = add_from_clause(add_select(query), "-")
     with tempfile.NamedTemporaryFile() as temp_file:
         connection = sqlite3.connect(temp_file.name)
         # Let SQLite tell me what tables I need to load by repeatedly running the query.
@@ -254,16 +286,18 @@ def execute_query(query: str, delimiter: str, max_columns: int, fixed_string: bo
             try:
                 cursor.execute(processed_query)
             except sqlite3.OperationalError as e:
-                no_such_table = 'no such table: '
+                no_such_table = "no such table: "
                 msg: str = e.args[0]
                 if msg.startswith(no_such_table):
-                    table_name = msg[len(no_such_table):]
+                    table_name = msg[len(no_such_table) :]
                     # SQLite treats "SELECT * FROM foo.log" as database foo, table log
                     # We could try to magically handle this, but SELECT * FROM "foo.log" gives the
                     # same message.
                     error = "Should have already loaded {}. You might need to quote the table name"
                     assert table_name not in loaded, error.format(table_name)
-                    load_file(connection, table_name, delimiter, max_columns, fixed_string)
+                    load_file(
+                        connection, table_name, delimiter, max_columns, fixed_string
+                    )
                     loaded.add(table_name)
                 else:
                     _logger.error("Failed to execute: %s", processed_query)
@@ -276,7 +310,7 @@ def execute_query(query: str, delimiter: str, max_columns: int, fixed_string: bo
 def print_output(rows: Cursor, delimiter: str, header: bool) -> None:
     def stringify(col: object) -> str:
         if col is None:
-            return 'NULL'
+            return "NULL"
         else:
             return str(col)
 
@@ -293,5 +327,5 @@ def print_output(rows: Cursor, delimiter: str, header: bool) -> None:
             raise
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
